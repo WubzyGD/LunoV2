8d9c6a4418e12cf80d20272651a1253c1e74506c
WubzyGD <wubzygd@gmail.com>
2022-03-09 20:57:20 -0600

    L.neko and blush fix

--- commands/social/neko.js
+++ commands/social/neko.js
@@ -4,26 +4,26 @@ const UserData = require('../../models/user');
 const makeId = require('../../util/makeid');
 
 module.exports = {
-    name: "blush",
-    help: "Let others know that someone made your cheeks red `{{p}}blush`.",
+    name: "neko",
+    help: "Look at some adorable cat people with `{{p}}neko`!",
     meta: {
         category: 'Social',
-        description: "Let others know that someone made your cheeks red",
-        syntax: '`blush`',
+        description: "Look at some adorable cat people!",
+        syntax: '`neko`',
         extra: null
     },
     async execute(message, msg, args, cmd, prefix, mention, client) {
-        let savess = await Saves.findOne({name: 'blush'}) ? await Saves.findOne({name: 'blush'}) : new Saves({name: 'blush'});
+        let savess = await Saves.findOne({name: 'neko'}) ? await Saves.findOne({name: 'neko'}) : new Saves({name: 'neko'});
         let saves = savess.saves;
         if (!args.length) {return message.channel.send({embeds: [new Discord.MessageEmbed()
-            .setTitle(`${message.guild ? message.member.displayName : message.author.username} blushes UwU`)
+            .setTitle(`Here's a cute neko ${message.guild ? message.member.displayName : message.author.username}!`)
             .setImage(String(Array.from(saves.values())[Math.floor(Math.random() * saves.size)]))
-            .setColor('ad0072')
+            .setColor('ff9292')
         ]});}
         if (['s', 'save', 'n', 'new', 'a', 'add'].includes(args[0].toLowerCase())) {
             if (!args[1]) {return message.channel.send('oi there cunt, give me a link of an image to add!');}
             let tu = await UserData.findOne({uid: message.author.id});
-            if ((!tu || !tu.developer) && !client.misc.savers.includes(message.author.id)) {return message.reply("You must be a Luno Developer in order to add new blush GIFs.");}
+            if ((!tu || !tu.developer) && !client.misc.savers.includes(message.author.id)) {return message.reply("You must be a Luno Developer in order to add new neko GIFs.");}
             let e = true;
             let id;
             while (e === true) {id = makeId(6); if (!saves.has(id)) {e = false;}}
@@ -31,7 +31,7 @@ module.exports = {
             saves.set(id, args.join(" ").trim());
             savess.saves = saves;
             savess.save();
-            return message.channel.send("Save added!");
+            return message.channel.send("More adorable nekos! Yay!");
         }
     }
 };

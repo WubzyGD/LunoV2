4c6884678c3c898fe8bc097f946e2f6ae0bf3d09
WubzyGD <wubzygd@gmail.com>
2024-03-16 03:15:38 -0600

    dev prefix

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -23,7 +23,13 @@ module.exports = async (client, message) => {
 
     if (message.guild && !message.member.permissions.has("SEND_MESSAGES")) {return undefined;}
 	
-    let prefix = message.guild ? client.guildconfig.prefixes.has(message.guild.id) ? client.guildconfig.prefixes.get(message.guild.id) !== null ? client.guildconfig.prefixes.get(message.guild.id) : 'l.' : 'l.' : 'l.';
+    let prefix = message.guild 
+        ? client.guildconfig.prefixes.has(message.guild.id) 
+            ? client.guildconfig.prefixes.get(message.guild.id) !== null 
+                ? client.guildconfig.prefixes.get(message.guild.id) 
+                : client.misc.config.dev ? 'l..' : 'l.' 
+            : client.misc.config.dev ? 'l..' : 'l.' 
+        : client.misc.config.dev ? 'l..' : 'l.';
 
 	let msg = message.content.toLowerCase().replace('\u200E', '');
 	let mention = message.mentions.users.first();
